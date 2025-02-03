1b32ded0ef68b836bc1c82c5050aed1b01c1d146
mehrshad boleyla <boleyla.mehrshad1@gmail.com>
2025-02-03 18:05:33 +0330

    config setting for app and static file

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mainapp',
 ]
 
 MIDDLEWARE = [
@@ -117,7 +118,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
+# LANGUAGE_CODE = 'fa-ir'
+# TIME_ZONE = 'Asia/Tehran'
+# USE_I18N = True
+# USE_TZ = True
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
